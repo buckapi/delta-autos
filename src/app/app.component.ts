bebd98322b30a0584d8d05d71529cb0aa2e5962c
buckapi <buckapis@gmail.com>
2025-03-24 09:36:47 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ScriptLoaderService } from './services/loader.service'; // Asegúrate de que la ruta sea correcta
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  title = 'delta-autos';
+  constructor(private scriptLoader: ScriptLoaderService) {}
+  ngOnInit() {
+    this.loadScripts();
+  }
+
+  loadScripts() {
+    const scripts = [
+      'app/js/jquery.min.js',
+      'app/js/jquery.easing.js',
+      'app/js/jquery.nice-select.min.js',
+      'app/js/bootstrap.min.js',
+      'app/js/plugin.js',
+      'app/js/shortcodes.js',
+      'app/js/main.js',
+      'app/js/swiper-bundle.min.js',
+      'app/js/swiper.js',
+      'app/js/jquery-validate.js',
+      // 'app/js/price-ranger.js' // Descomentar si se necesita
+    ];
+
+    const scriptPromises = scripts.map(script => this.scriptLoader.loadScript(script));
+    
+    Promise.all(scriptPromises)
+      .then(() => {
+        console.log('All scripts loaded successfully');
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+}
